37ec0ea20e48199d51f9324824774ef0370c427a
Stephen Roberts <stephen.roberts@anu.edu.au>
2017-01-23 17:15:59 +1100

    Setup utils dir

--- anuga_1d/utilities/setup.py
+++ anuga_1d/utilities/setup.py
@@ -14,20 +14,17 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
 
     
-    config = Configuration('channel', parent_package, top_path)
+    config = Configuration('utilities', parent_package, top_path)
 
     config.add_data_dir('tests')
 
-    #util_dir = os.path.abspath(join(os.path.dirname(__file__),'..','utilities'))
-    #runtime_dir = os.path.abspath(join(os.path.dirname(__file__),'..','runtime_libs')) 
-   
     util_dir = join('..','utilities') 
  
     
-    config.add_extension('channel_domain_ext',
-                         sources=['channel_domain_ext.c'],
-                         include_dirs=[util_dir])        
-    
+    config.add_extension('util_ext',
+                         sources=['util_ext.c'],
+                         include_dirs=[util_dir])
+
 
     return config
     
